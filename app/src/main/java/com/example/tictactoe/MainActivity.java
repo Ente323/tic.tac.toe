b71f62c3dd27f877e3a4c946fd20b8466054f82d
carst <carsten.mohsmann@gmail.com>
2023-11-21 09:12:01 +0100

    Carsten ist ein Hurensohn 3.3

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-//Hurensohn 3.1
+//Hurensohn 3.3
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
